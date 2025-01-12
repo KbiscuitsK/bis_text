34dcf668b5e2710814ff75f74dacf38b16d7ba22
qfisking <2539120993@qq.com>
2025-01-12 11:29:07 +0800

    添加Author信息

--- manage-api-spring-common/src/main/java/nxu/it/movieapi/service/AuthorService.java
+++ manage-api-spring-common/src/main/java/nxu/it/movieapi/service/AuthorService.java
@@ -10,6 +10,12 @@ public interface AuthorService {
     default List<Author> findAll(){
         List<Author> authorList = new ArrayList<>();
         authorList.add(new Author("12022246775","郭智成",2022,"计4"));
+        authorList.add(new Author("12022246857","毛自鹏",2022,"计4"));
+        authorList.add(new Author("12022246770","罗学仙",2022,"计4"));
+        authorList.add(new Author("12022246858","马嘉骏",2022,"计4"));
+        authorList.add(new Author("12022246837","买金虎",2022,"计4"));
+        authorList.add(new Author("12022246723","邓子鹏",2022,"计4"));
+        authorList.add(new Author("12022246864","孔德豪",2022,"计4"));
         return authorList;
         };
 }
