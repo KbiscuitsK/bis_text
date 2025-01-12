c31acc84b0b4af0bf4e3d9f80d0c3a663d4e022e
qfisking <2539120993@qq.com>
2025-01-12 11:39:35 +0800

    删除冗余文件 更改Author实现类

--- movie-api-service-spring-pg-impl/src/main/java/nxu/it/movieapi/service/pg/AuthorServicePgImpl.java
+++
@@ -1,7 +0,0 @@
-package nxu.it.movieapi.service.pg;
-
-
-import nxu.it.movieapi.service.AuthorService;
-
-public class AuthorServicePgImpl implements AuthorService {
-}
--- movie-api-spring-web/src/main/java/nxu/it/movieapi/Impl/AuthorServiceDefaultImpl.java
+++ movie-api-spring-web/src/main/java/nxu/it/movieapi/Impl/AuthorServiceDefaultImpl.java
@@ -8,13 +8,4 @@ import java.util.List;
 
 @Component
 public class AuthorServiceDefaultImpl implements AuthorService {
-    @Override
-    public List<Author> findAll() {
-        List<Author> authorList=List.of(
-                new Author("001","赵大",2022,"1班"),
-                new Author("002","王二",2022,"2班"),
-                new Author("003","张三",2022, "3班"),
-                new Author("004","李四",2022,"4班"));
-        return authorList;
-    }
 }
